54fd5aeefebd427c0006e504d10b99f3d96ac14d
Ben Heavner <bheavner@gmail.com>
2016-08-02 11:33:15 -0700

    First commit - makefiles for ADNI_GO_2 QC

---
+++ ADNI_GO_2/config.mk
@@ -0,0 +1,5 @@
+PLINK_EXE=/local/tools/plink-1.90/plink
+BFILE=/ADNI_GO_2_Forward
+DATA_DIR=/local/bdds/data/ADNI/snp_data/ADNI_GO_2/ADNI_GO_2_OmniExpress
+WORKING_DIR=/local/bdds/data/ADNI/snp_data/ADNI_GO_2/QC
+
